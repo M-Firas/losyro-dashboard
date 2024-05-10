84440672de076ee5b8925258fec41f504b9a978e
M-Firas <firasahmad99916@hotmail.com>
2024-05-10 18:52:24 +0300

    second commit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,39 +1,30 @@
-import "../Login/Login.css"
-import logo from '../../images/losyro-icon.svg'
+import "../Login/Login.css";
 
 const Login = () => {
-    return (
-        <div className="login">
-          <div className="login--conatiner">
-            <h3 className="title"><img src={logo} alt=""/>LOSYRO</h3>
+  return (
+    <div className="login--conatiner">
+      <form>
+        <h2>Login</h2>
+        <input type="text" placeholder="Email Address"></input>
+        <input type="password" placeholder="Password"></input>
 
-            <form>
-              <h2>Login</h2>
-              <input type="text" placeholder="Email Address"></input>
-              <input type="password" placeholder="Password"></input>
-
-              <div className="login--options">
-              <div className="remember-me">
-              <input className="checkbox" type="checkbox" id="remember-me"/>
-              <label for='remember-me'>Remember Me</label>
-              </div>
-              <a href="">Forgot Password</a>
-              </div>
-
-              <button>Log In</button>
-              
-              <div className="signup">
-                <p>Don’t have an account yet?</p>
-                <a href="">Create account</a>
-              </div>
-
-
-            </form>
-
-
+        <div className="login--options">
+          <div className="remember-me">
+            <input className="checkbox" type="checkbox" id="remember-me" />
+            <label for="remember-me">Remember Me</label>
           </div>
+          <a href="">Forgot Password</a>
         </div>
-      );
-}
- 
+
+        <button>Log In</button>
+
+        <div className="signup">
+          <p>Don’t have an account yet?</p>
+          <a href="">Create account</a>
+        </div>
+      </form>
+    </div>
+  );
+};
+
 export default Login;
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,19 @@
 import "./LoginPage.css"
-import Login from '../../components/Login/Login'
+import backgroundImage from '../../images/Group 1000001645.png'
 import logo from '../../images/losyro-icon.svg'
+import Login from '../../components/Login/Login'
+
 
 const LoginPage = () => {
     return (
         <div className="login--page">
-        <Login></Login>
+        <div className="login--left">
+        <img src={backgroundImage} alt="" />
+        </div>
+        <div className="login--right">
+        <h3 className="title"><img src={logo} alt=""/>LOSYRO</h3>
+        <Login />
+        </div>
         </div>
       );
 }
